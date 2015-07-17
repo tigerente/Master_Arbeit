33502baccf959c83e23a9f895a75e6fb6126b913
Sascha <hallosascha@hotmail.de>
2015-07-17 18:52:00 +0200

    first graphic with worst, average and best case

--- R/Fallstudie_Grafik_Nutzungsintensivierung_Analyse.R
+++ R/Fallstudie_Grafik_Nutzungsintensivierung_Analyse.R
@@ -18,55 +18,101 @@ if(useTikz) tikz( '../tex/Abbildungen/Fallstudie_Nutzungsintensivierung.tex', pa
 
 #### Konstanten ####
 var = 0.1      # Relative Abweichung für Sensititvitäts-Analyse
+manipulate.function <- function(var){
 low = 1 - var
 mid = 1
 hig = 1 + var
 
+#### Parametersätze ####
+Avg <- data.frame(
+  # n_max = n_max_mid[1],
+  # t_max = t_max_mid[1],
+  n_max = n_max_mid[1]*mid,
+  t_max = t_max_mid[1]*mid,
+  A     = A[1]*mid,
+  N     = N[1]*mid,
+  i_P   = i_P["MF","values"]*mid,
+  i_d   = i_d["MF","values"]*mid,
+  i_N   = i_N["MF","values"]*mid,
+  theta = theta[1]*mid
+  )
+Wor <- data.frame(
+  # n_max = n_max_low[1],
+  # t_max = t_max_hig[1],
+  n_max = n_max_mid[1]*low,
+  t_max = t_max_mid[1]*hig,
+  A     = A[1]*hig,
+  N     = N*hig,
+  i_P   = i_P["MF","values"]*low,
+  i_d   = i_d["MF","values"]*hig,
+  i_N   = i_N["MF","values"]*hig,
+  theta = theta[1]*hig
+  )
+Bes <- data.frame(
+  # n_max = n_max_hig[1],
+  # t_max = t_max_low[1],
+  n_max = n_max_mid[1]*hig,
+  t_max = t_max_mid[1]*low,
+  A     = A[1]*low,
+  N     = N*low,
+  i_P   = i_P["MF","values"]*hig,
+  i_d   = i_d["MF","values"]*low,
+  i_N   = i_N["MF","values"]*low,
+  theta = theta[1]*low
+  )
 
 #### Teil 1: Kriterien überprüfen ####
 # h* berechnen #
-h_stern_low = n_max_low[1]/t_max_hig[1]
-h_stern_mid = n_max_mid[1]/t_max_mid[1]
-h_stern_hig = n_max_hig[1]/t_max_low[1]
+h_stern_Wor = with(Wor, n_max/t_max)
+h_stern_Avg = with(Avg, n_max/t_max)
+h_stern_Bes = with(Bes, n_max/t_max)
 
-p_stern_low = floor(N*low / (h_stern_hig * Time))
-p_stern_mid = floor(N*mid / (h_stern_mid * Time))
-p_stern_hig = floor(N*hig / (h_stern_low * Time))
+p_stern_Wor = with(Wor, floor(N / (h_stern_Wor * Time)))
+p_stern_Avg = with(Avg, floor(N / (h_stern_Avg * Time)))
+p_stern_Bes = with(Bes, floor(N / (h_stern_Bes * Time)))
 
 p_Ind = Szenario.Ind["p","values"]
-h_Ind_low = h(p_Ind, N*low, Time)
-h_Ind_mid = h(p_Ind, N*mid, Time)
-h_Ind_hig = h(p_Ind, N*hig, Time)
+h_Ind_Wor = with(Wor, h(p_Ind, N, Time))
+h_Ind_Avg = with(Avg, h(p_Ind, N, Time))
+h_Ind_Bes = with(Bes, h(p_Ind, N, Time))
 
 # Ausgabe #
-overview <- data.frame(h_Ind    = c(h_Ind_low,h_Ind_mid,h_Ind_hig),
-                       h_stern  = c(h_stern_low,h_stern_mid,h_stern_hig),
+overview <- data.frame(h_Ind    = c(h_Ind_Wor,h_Ind_Avg,h_Ind_Bes),
+                       h_stern  = c(h_stern_Wor,h_stern_Avg,h_stern_Bes),
                        p_Ind    = rep(p_Ind, times=3),
-                       p_stern  = c(p_stern_hig,p_stern_mid,p_stern_low))
+                       p_stern  = c(p_stern_Wor,p_stern_Avg,p_stern_Bes))
 overview$check <- overview$h_Ind < overview$h_stern
 row.names(overview) <- c("worst", "average", "best")
 
 print("Teil 1: Kriterienüberprüfung:")
 print(overview, digits=0)
 
-#### Teil 2: Delta MIPS (h_gem) ####
-delta_MIPS <- function(p, t_max = t_max_mid[1], n_max = n_max_mid[1]){
-  MIPS_Ind <- MIPS(h = h(p_Ind, N, Time), I_fix = 0, S_D = S_D[1], i_P = i_P["MF","values"],
-                   t_max = t_max, A = A[1], n_max = n_max)  
+#### Teil 2: Delta MIPS (p, daten) ####
+delta_MIPS <- function(p, daten){
+  MIPS_Ind <- with(daten, MIPS(h = h(p_Ind, N, Time), I_fix = i_N*N, S_D = N*A, i_P = i_P,
+                   t_max = t_max, A = A, n_max = n_max))
   
-  MIPS_Gem <- MIPS(h = h(p, N, Time), I_fix = 0, S_D = S_D[1], i_P = i_P["MF","values"],
-                   t_max = t_max, A = A[1], n_max = n_max)  
-  
-  return(MIPS_Gem - MIPS_Ind)
+  MIPS_Gem <-  with(daten, MIPS(h = h(p, N, Time), I_fix = i_N*N, S_D = N*A, i_P = i_P,
+                   t_max = t_max, A = A, n_max = n_max))
+  return((MIPS_Gem - MIPS_Ind)/(MIPS_Ind))
 }
 
 
 p_min = 2
-p_dis <- seq(from=p_Ind, to=p_min, by=-1)
-p_con <- seq(from=p_Ind, to=p_min, length.out=200)
+p_dis <- seq(from=p_Ind, to=p_min, by=-1) # ganzzahlige p-Werte
+p_con <- seq(from=p_Ind, to=p_min, length.out=200) # "kontinuierliche" p-Werte
 
-plot(p_dis, delta_MIPS(p_dis), xlim=rev(range(p_dis)), type = 'p', pch = 22, bg = 'white',
-     panel.first = lines(p_con, delta_MIPS(p_con), type = 'l', lty = 1, col='grey'))
+plot(p_dis, delta_MIPS(p_dis, Bes), xlim=rev(range(p_dis)), type = 'p', pch = 22, bg = 'white',
+     panel.first = lines(p_con, delta_MIPS(p_con, Bes), type = 'l', lty = 1, col='grey'), ylim=c(-0.2,0))
+
+points(p_dis, delta_MIPS(p_dis, Avg), xlim=rev(range(p_dis)), type = 'p', pch = 22, bg = 'white',
+     panel.first = lines(p_con, delta_MIPS(p_con, Avg), type = 'l', lty = 1, col='grey'))
+
+points(p_dis, delta_MIPS(p_dis, Wor), xlim=rev(range(p_dis)), type = 'p', pch = 22, bg = 'white',
+     panel.first = lines(p_con, delta_MIPS(p_con, Wor), type = 'l', lty = 1, col='grey'))
+}
+manipulate(manipulate.function(var),
+           var=slider(0,0.5,0.01))
 
 
 
