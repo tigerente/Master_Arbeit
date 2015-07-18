596f090b20d80ec3b25d202861f77e02724da462
Sascha <hallosascha@hotmail.de>
2015-07-19 00:19:38 +0200

    erste Entwürfe für die restlichen Graphiken der Fallstudie

--- R/Fallstudie_Grafik_Nutzungsintensivierung_Analyse.R
+++ R/Fallstudie_Grafik_Nutzungsintensivierung_Analyse.R
@@ -1,5 +1,6 @@
 # Funktionsdefinition einbinden:
 source('./Funktionen_Nutzungsintensivierung.R')
+source('./Funktionen_Transporte.R')
 source('./Fallstudie_data.R')
 
 # für den Export von Graphiken in tex-Files
@@ -8,6 +9,7 @@ require(manipulate)
 
 # Einstellungen für den Export der Graphik
 tikzheight <- 4.0
+
 tikzwidth <- 5.8
 fntsize <- 0.8
 
@@ -72,6 +74,7 @@ p_stern_Avg = with(Avg, floor(N / (h_stern_Avg * Time)))
 p_stern_Bes = with(Bes, floor(N / (h_stern_Bes * Time)))
 
 p_Ind = Szenario.Ind["p","values"]
+p_Gem = Szenario.Luh["p","values"]
 h_Ind_Wor = with(Wor, h(p_Ind, N, Time))
 h_Ind_Avg = with(Avg, h(p_Ind, N, Time))
 h_Ind_Bes = with(Bes, h(p_Ind, N, Time))
@@ -116,9 +119,55 @@ manipulate(manipulate.function(var),
 
 
 
+##################
+#### Kopplung ####
+##################
+
+#### Teil 1: delta_MIPS in Abhängigkeit von d ####
+delta_MIPS <- function(d, p, daten){
+  MIPS_Ind <- with(daten, MIPS(h = h(p_Ind, N, Time), I_fix = i_N*N, S_D = N*A, i_P = i_P,
+                   t_max = t_max, A = A, n_max = n_max))
+  
+  MIPS_Gem <-  with(daten, MIPS(h = h(p, N, Time), I_fix = i_N*N, S_D = N*A, i_P = i_P,
+                   t_max = t_max, A = A, n_max = n_max))
+  MIPS_Tra <- with(daten, MIPS_Gem + MIPS_a(d = d, theta = theta, i_d = i_d, 
+                    a = A, K = 1, I_fix = 0, S_D = N*A))
+  return((MIPS_Tra - MIPS_Ind)/(MIPS_Ind))
+}
+
+d_seq <- seq(from=0, to=5, length.out=200) 
+
+plot(d_seq, -1 * delta_MIPS(d = d_seq, p = Szenario.Tra["p", "values"], daten = Bes), type = 'l') 
+abline(h=0, col="grey")
 
 
 
+
+#### Teil 2: Überprüfung von d < d* ####
+
+
+#### Teil 3: Szenarien vergleichen ####
+MIPS_N_I <- with(Bes, i_N/A)
+MIPS_N_II <- with(Bes, i_N/A)
+MIPS_P_I <- with(Bes, MIPS(h = h(p_Ind, N, Time), I_fix = 0, S_D = N*A, i_P = i_P,
+                                                t_max = t_max, A = A, n_max = n_max))
+MIPS_P_II <- with(Bes, MIPS(h = h(p_Gem, N, Time), I_fix = 0, S_D = N*A, i_P = i_P,
+                                                t_max = t_max, A = A, n_max = n_max))
+MIPS_T <- with(Bes, MIPS_a(d = Szenario.Tra["d", "values"], theta = theta, i_d = i_d, 
+                    a = A, K = 1, I_fix = 0, S_D = N*A))
+
+MIPS_Vergleich <- matrix(c(MIPS_N_I, MIPS_P_I, 0, MIPS_N_II, MIPS_P_II, 0, MIPS_N_II, MIPS_P_II, MIPS_T), nrow=3, ncol=3)
+dimnames(MIPS_Vergleich) <- list(c("N","P", "T"), c("Szenario I", "Szenario II", "Szenario III"))
+
+barplot(MIPS_Vergleich, col=c("gray75", "gray85","gray95"), ylim=c(0,ceiling(MIPS_N_I+MIPS_P_I + MIPS_T)))
+axis (side = 4, at = c(0,0.25,0.5,0.75,1)*(MIPS_N_I+MIPS_P_I), labels = paste(c(0,25,50,75,100), "%"))
+legend("topright",c("N","P", "T"))
+
+#### Teil 4: Plot d~p ####
+require(plot3D)
+image2D(x=p_con, y=d_seq, z=outer(p_con, d_seq, FUN = function(p,d) delta_MIPS(p=p, d=d, daten=Bes)), col=grey(c(seq(0.7,1,length.out=147), seq(1,0.7
+                                                                                                                                    ,length.out=200))))
+contour2D(x=p_con, y=d_seq, z=outer(p_con, d_seq, FUN = function(p,d) delta_MIPS(p=p, d=d, daten=Bes)), add=TRUE, col="black")
 I_N     <- i_N["MF","values"] * N
 
 # spezifische MIPS-Funktion:
